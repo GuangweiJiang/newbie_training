1ba9a412b8ce2c44eaaae99e07a9a04a93a61457
AllanXing <william@william-wu.(none)>
2012-09-18 15:34:03 +0800

    Owner: Allan xing
    Issue: N/A
    
    Description: Weekly_program
    
    Root Cause: N/A

--- weekly_programming/ex02/ex02.c
+++ weekly_programming/ex02/ex02.c
@@ -1,12 +1,13 @@
 /*
  * file name: ex02.c
- * pupose: Input a intger,then get how many "1" of it's binary form
+ * purpose: Input a intger,then get how many "1" of it's binary form
  * creator: Allan xing
  * create time: 2012-09-17
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+
 
 int Bget(int n)
 {
