76f29e56fdb55f34648a39e6528897835093ccac
AllanXing <william@william-wu.(none)>
2012-09-18 17:03:11 +0800

    Description: A simple driver of char Owner: Allan xing Issue: N/A Root Cause: N/A

--- linux_android_basic_training/linux_drivers_training/ex_02/ex_02.c
+++ linux_android_basic_training/linux_drivers_training/ex_02/ex_02.c
@@ -1,3 +1,10 @@
+/*
+ * file name: ex_02.c
+ * purpose: a simple driver of char
+ * creator: Allan xing
+ * create time: 2012-09-17
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/cdev.h>
@@ -9,65 +16,81 @@ unsigned int minor = 0;
 dev_t devno;
 
 struct cdev test_cdev;
-struct file_operations test_fops;
-/*
-static int char_open(void)
+
+static int char_open(struct inode *node, struct file *file)
+{
+	printk("open device!\n");
+	return 0;
+}
+static int char_close(struct inode *node, struct file *file)
+{
+	printk("close device!\n");
+	return 0;
+}
+
+static ssize_t char_read(struct file *file,char __user* buffer,size_t count,loff_t *offset)
 {
 	return 0;
 }
-static int char_close(void)
+static ssize_t char_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
+{
+	return 0;
+}
+static long char_ioctl(struct file *file,unsigned int cmd,unsigned long arg)
 {
 	return 0;
 }
 
-static ssize_t char_read(void)
-{
-	return 0;
-}
-static ssize_t char_write(void)
-{
-	return 0;
-}
-
-static long char_iotcl(void)
-{
-	return 0;
-}
-
-static const struct file_operation test_fops = {
+static const struct file_operations test_fops = {
 	.open		= char_open,
-	.close		= char_close,
+	.release	= char_close,
 	.read		= char_read,
 	.write		= char_write,
-	.compat_iotcl=char_iotcl,
+	.compat_ioctl		=char_ioctl,
 	.owner		= THIS_MODULE
 };
-*/
+
 static int __init char_init(void)
 {
-	//1 登记设备号
+	int ret=0;
+	//1 register device
 	if(major){
 		devno = MKDEV(major,minor);
-		register_chrdev_region(devno,1,"chardev");
+		ret = register_chrdev_region(devno,1,"chardev");
 	}else{
-		alloc_chrdev_region(&devno,minor,1,"chardev");
+		ret = alloc_chrdev_region(&devno,minor,1,"chardev");
+		major = MAJOR(devno);
+		minor = MINOR(devno);
 	}
-	return 0;
-	//2 注册设备
-	//初始化设备
+	if(ret<0){
+		printk("register error!\n");
+		goto err0;
+	}
+	printk("major=%d,minor=%d\n",major,minor);
+	//2 register device
+	//initialize device
 	cdev_init(&test_cdev,&test_fops);
-	//添加cdev到内核
-	cdev_add(&test_cdev,devno,1);
+	//add cdev to kernel
+	ret = cdev_add(&test_cdev,devno,1);
+	if(ret<0){
+		printk("add  error!\n");
+		goto err1;
+	}
 
 	printk("Hello kernel\n");
 	return 0;
+
+err1:
+	unregister_chrdev_region(devno,1);//if Registration failed,unregister it
+err0:
+	return ret;
 }
 
 static void __exit char_exit(void)
 {
-	//1 从内核中删除cdev
+	//1 delete the cdev from kernel
 	cdev_del(&test_cdev);
-	//2 注销设备号
+	//2 Cancellation of the device number
 	unregister_chrdev_region(devno,1);
 
 	printk("Good bye kernel\n");
