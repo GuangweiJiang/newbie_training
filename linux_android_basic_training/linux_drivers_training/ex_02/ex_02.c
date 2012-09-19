d36a505349bbdb7a9864898a7517a58b2adb706b
AllanXing <william@william-wu.(none)>
2012-09-19 15:52:00 +0800

    Description: A simple driver of char Issue: N/A Owner: Allan xing Root Cause: N/A

--- linux_android_basic_training/linux_drivers_training/ex_02/ex_02.c
+++ linux_android_basic_training/linux_drivers_training/ex_02/ex_02.c
@@ -3,6 +3,7 @@
  * purpose: a simple driver of char
  * creator: Allan xing
  * create time: 2012-09-17
+ * modify time: 2010-09-19
  */
 
 #include <linux/init.h>
@@ -10,12 +11,25 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
 #include <linux/cdev.h>
+#include <linux/sched.h>
+#include <linux/types.h>
+#include <linux/memory.h>
+#include <asm/system.h>
+#include <asm/uaccess.h>
 
-unsigned int major = 0;
-unsigned int minor = 0;
+#define DEV_NAME 	"chardev"
+#define MEM_SIZE 	0x1000//The size of the global memory  
+#define MAJOR_NUM 	0
+#define MINOR_NUM 	0
+
+//make device structure
+struct global_dev{
+	struct cdev test_cdev;
+	char 	memory[MEM_SIZE];
+};
+struct global_dev gdev;
 dev_t devno;
 
-struct cdev test_cdev;
 
 static int char_open(struct inode *node, struct file *file)
 {
@@ -30,12 +44,43 @@ static int char_close(struct inode *node, struct file *file)
 
 static ssize_t char_read(struct file *file,char __user* buffer,size_t count,loff_t *offset)
 {
-	return 0;
+	unsigned long p = *offset;
+	int ret = 0;
+	int nread;
+	if(p >= MEM_SIZE)
+		return 0;
+	if(count > MEM_SIZE - p)
+		count = MEM_SIZE - p;
+	nread = copy_to_user(buffer,gdev.memory + p, count);
+	if(nread>0)
+		ret = count - nread;
+	else{
+		p += count;
+		ret = count;
+	}
+	printk("Read %ld bytes from %ld\n",count,p);
+	return ret;
 }
-static ssize_t char_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
+static ssize_t char_write(struct file *file,const char __user* buffer,size_t count,loff_t *offset)
 {
-	return 0;
+	unsigned long p = *offset;
+	int ret = 0;
+	int nwrite;
+	if(p >= MEM_SIZE)
+		return 0;
+	if(count > MEM_SIZE - p)
+		count = MEM_SIZE - p;
+	nwrite = copy_from_user(gdev.memory + p,buffer, count);
+	if(nwrite>0)
+		ret = count - nwrite;
+	else{
+		p += count;
+		ret = count;
+	}
+	printk("Write %ld bytes from %ld\n",count,p);
+	return ret;
 }
+
 static long char_ioctl(struct file *file,unsigned int cmd,unsigned long arg)
 {
 	return 0;
@@ -54,34 +99,31 @@ static int __init char_init(void)
 {
 	int ret=0;
 	//1 register device
-	if(major){
-		devno = MKDEV(major,minor);
-		ret = register_chrdev_region(devno,1,"chardev");
+	if(MAJOR_NUM){
+		devno = MKDEV(MAJOR_NUM,MINOR_NUM);
+		ret = register_chrdev_region(devno,1,DEV_NAME);
 	}else{
-		ret = alloc_chrdev_region(&devno,minor,1,"chardev");
-		major = MAJOR(devno);
-		minor = MINOR(devno);
+		ret = alloc_chrdev_region(&devno,MINOR_NUM,1,DEV_NAME);
 	}
 	if(ret<0){
 		printk("register error!\n");
 		goto err0;
 	}
-	printk("major=%d,minor=%d\n",major,minor);
+	printk("major=%d,minor=%d\n",MAJOR(devno),MINOR(devno));
 	//2 register device
 	//initialize device
-	cdev_init(&test_cdev,&test_fops);
+	cdev_init(&gdev.test_cdev,&test_fops);
 	//add cdev to kernel
-	ret = cdev_add(&test_cdev,devno,1);
+	ret = cdev_add(&gdev.test_cdev,devno,1);
 	if(ret<0){
 		printk("add  error!\n");
 		goto err1;
 	}
-
-	printk("Hello kernel\n");
+	printk("Hello kernel!\n");
 	return 0;
 
 err1:
-	unregister_chrdev_region(devno,1);//if Registration failed,unregister it
+	unregister_chrdev_region(devno,1);//if registration failed,unregister it
 err0:
 	return ret;
 }
@@ -89,11 +131,11 @@ err0:
 static void __exit char_exit(void)
 {
 	//1 delete the cdev from kernel
-	cdev_del(&test_cdev);
+	cdev_del(&gdev.test_cdev);
 	//2 Cancellation of the device number
 	unregister_chrdev_region(devno,1);
 
-	printk("Good bye kernel\n");
+	printk("Good bye kernel!\n");
 }
 
 module_init(char_init);
