a16e23958d9cf1d39c2f222ff6f7b3aa5c2a70be
AllanXing <william@william-wu.(none)>
2012-09-19 15:54:17 +0800

    Description: A test program Issue: N/A Owner: Allan xing Root Cause: N/A

---
+++ linux_android_basic_training/linux_drivers_training/ex_02/write_t.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+
+int main(int argc,char** argv)
+{
+	if(argc<3){
+		fprintf(stderr,"Usage:%s\n",strerror(errno));
+		exit(0);
+	}
+	int fd;
+	if((fd=open(argv[1],O_RDWR | O_NONBLOCK | O_ASYNC))<0){
+		fprintf(stderr,"open file error:%s\n",strerror(errno));
+		exit(0);
+	}
+	write(fd,argv[2],strlen(argv[2]));
+	printf("write: str= %s\n",argv[2]);
+	close(fd);
+	return 0;
+}
