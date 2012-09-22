e682208fad08cd9dbf780e0f2cf68ea3334e910a
xingpeng <xingpeng@ubuntu.ubuntu-domain>
2012-09-22 19:24:36 +0800

    Description: weekly program Issue: N/A Owner: Allan xing Root Cause: N/A

--- weekly_programming/ex02/ex02.c
+++ weekly_programming/ex02/ex02.c
@@ -3,8 +3,8 @@
  * purpose: Input a intger,then get how many "1" of it's binary form
  * creator: Allan xing
  * create time: 2012-09-17
- * modify time: 2012-09-20 10:10
- * modify time: 2012-09-20 15:43
+ * modify time: 2012-09-20 consummate function
+ * modify time: 2012-09-21 consummate error information
  */
 
 #include <stdio.h>
@@ -12,7 +12,7 @@
 #include <string.h>
 
 
-int Bget(int n)
+int Bget1(int n)
 {
 	int count=0;
 	while(n)
@@ -23,33 +23,60 @@ int Bget(int n)
 	return count;
 }
 
+int Bget2(int n)
+{
+	int m=1;
+	int count=0;
+	while(m)
+	{
+		if(n & m)
+			count++;
+			m <<= 1;
+	}
+	return count;
+}
+
 int IsNum(char *str)
 {
 	int i=0,num;
 	int isNUM=1;
 	while(str[i]!='\0'&isNUM)
+	{
 		if((str[i]>='0'&&str[i]<='9')||(str[0]=='-'&&str[i+1]>='0'&&str[i+1]<='9'))
 		{
 			i++;
 		}else{
 			isNUM=0;
 		}
-	if(isNUM)
-	{
-		int m=atoi(str);//Convert a string to an integer
-		return m;
-	}else{
-		printf("input error!\n");
-		exit(0);
-	}	
-
+		if(isNUM)
+		{
+			return 1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
 }
 
 int main(void)
 {	
 	char str[100];
-	printf("input a intger: ");
-	scanf("%s[^\n]",str);
-	printf("It has %d '1'\n",Bget(IsNum(str)));
+	while(str!=NULL)
+	{
+		printf("input a intger: ");
+		scanf("%s[^\n]",str);
+		if(IsNum(str))
+		{
+			int m=atoi(str);
+			printf("It has %d '1'\n",Bget1(m));
+			break;
+		}
+		else
+		{
+			printf("Input error,try again!\n");
+			continue;
+		}
+	}
 	return 0;
 }
