e0247934315425fcce6f5e14ee3e8b3cbb7e8f28
longbin <longbin@ubuntu.(none)>
2014-07-04 16:59:28 +0800

    modify test procedure

--- linux_android_basic_training/linux_drivers_training/ex_02/test.sh
+++ linux_android_basic_training/linux_drivers_training/ex_02/test.sh
@@ -5,15 +5,17 @@ gcc -o user.o user.c
 #clear
 echo "**make OK**"
 file=`ls *.ko`
-echo "	Press <ENTER> to insert $file module"
-read
-sudo insmod $file && echo "#insmod $file"
-echo 'cat /proc/devices | grep "scull"' && cat /proc/devices | grep 'scull'
-sudo chmod 666 /dev/scull0
+read -p "	Press <ENTER> to insert $file module"
+echo '#sudo insmod $file' && sudo insmod $file
+echo '#cat /proc/devices | grep "scull"' && cat /proc/devices | grep 'scull'
+echo '#sudo chmod 666 /dev/scull0' && sudo chmod 666 /dev/scull0
 ls -l /dev/scull*
-echo "	Press <ENTER> to remove $file module"
-read
-sudo rmmod $file && echo "#rmmod $file"
-echo "	Press <ENTER> to continue"
-read
+echo '#echo "test" > /dev/scull0' && echo "test" > /dev/scull0
+echo '#cat /dev/scull0' && cat /dev/scull0
+echo '------------./user.o-------------'
+./user.o
+echo '---------------------------------'
+read -p "	Press <ENTER> to remove $file module"
+echo '#sudo rmmod $file' && sudo rmmod $file
+read -p "	Press <ENTER> to continue"
 sudo dmesg
