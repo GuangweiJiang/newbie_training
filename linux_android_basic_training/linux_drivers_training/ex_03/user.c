38814a2d0ef66d38bd322a42cc2d9880c28c60fb
longbin <longbin@ubuntu.(none)>
2014-07-08 17:33:35 +0800

    debug

--- linux_android_basic_training/linux_drivers_training/ex_03/user.c
+++ linux_android_basic_training/linux_drivers_training/ex_03/user.c
@@ -1,6 +1,6 @@
 /*
  *	filename:		user.c
- *	Description:	To test the misc device driver
+ *	Description:	To test the char device driver
  *	Author:			Longbin Li <beangr@163.com>
  */
 #include <stdio.h>
@@ -9,8 +9,8 @@
 #include <sys/types.h>
 #include <fcntl.h>
 
-#define MISC_READ	0X10001
-#define MISC_WRITE	0X10002
+#define SCULL_READ	0X10001
+#define SCULL_WRITE	0X10002
 
 int main(int argc, char *argv[]){
 	int rdata;
@@ -25,18 +25,22 @@ int main(int argc, char *argv[]){
 	char buf1[50] = "hello hello how are you";
 	char buf2[50];
 
-	write(fd, buf1, 50);
-	lseek(fd, 0, SEEK_SET);
-	read(fd, buf2, 50);
-	printf("%s\n", buf2);
+	if (write(fd, buf1, 50) != -1){
+		lseek(fd, 0, SEEK_SET);
+		if (read(fd, buf2, 50) != -1){
+			printf("%s\n", buf2);
+		}
+	}
 
-	ioctl(fd, MISC_READ, &rdata);
-	printf("ioctl:read = %#x\n", rdata);
-	sleep(1);
+	if (ioctl(fd, SCULL_READ, &rdata) != -1) {
+		printf("read:%#x\n", rdata);
+		sleep(1);
+	}
 
-	printf("ioctl:write %#x to dev\n", wdata);
-	ioctl(fd, MISC_WRITE, &wdata);
-	sleep(1);
+	if (ioctl(fd, SCULL_WRITE, &wdata) != -1) {
+		printf("write:%#x\n", wdata);
+		sleep(1);
+	}
 
 	return 0;
 }
