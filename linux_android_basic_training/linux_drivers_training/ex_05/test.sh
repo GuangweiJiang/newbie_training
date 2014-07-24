608ea271458014d1abb48fc37abdc636b44330aa
lilongbin <837839984@qq.com>
2014-07-24 15:55:50 +0800

    update

--- linux_android_basic_training/linux_drivers_training/ex_05/test.sh
+++ linux_android_basic_training/linux_drivers_training/ex_05/test.sh
@@ -6,14 +6,14 @@ gcc -o user.o user.c
 echo "**make OK**"
 file=`ls *.ko`
 read -p "	Press <ENTER> to insert $file module"
-echo '#sudo insmod $file' && sudo insmod $file
+echo "#sudo insmod $file" && sudo insmod $file
 echo '#cat /proc/devices | grep "scull"' && cat /proc/devices | grep 'scull'
 echo '#sudo chmod 666 /dev/scull0' && sudo chmod 666 /dev/scull0
 ls -l /dev/scull*
-echo '#echo "test" > /dev/scull0' && echo "test" > /dev/scull0
-echo '#cat /dev/scull0' && cat /dev/scull0
+#echo '#echo "test" > /dev/scull0' && echo "test" > /dev/scull0
+#echo '#cat /dev/scull0' && cat /dev/scull0
 echo '------------./user.o-------------'
-./user.o
+./user.o 
 echo '---------------------------------'
 read -p "	Press <ENTER> to remove $file module"
 echo '#sudo rmmod $file' && sudo rmmod $file
