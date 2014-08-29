f2ca4172f2d0ed73fa98f178bc7f16c8f7ed6a7b
jimmy-jiangzh <164742466@qq.com>
2014-08-29 09:28:36 +0800

    0828 daily report

---
+++ daily_report/jimmy_0828.c
@@ -0,0 +1,7 @@
+  Today is Thursday.The weather is cool.
+  In the morning,Jerry teachs me how to update touch FW with the config modified.
+We need to help the PM update the touch image.The engineer in taibei often can 
+modify the config that can make the touch  work better.It has two modes.one is 
+stylus,another is glove.I need to update it without delay.
+  In the afternoon,Guangwei teachs us some important knowledge about gpio.I learn a lot.
+Then I write a C program code.
